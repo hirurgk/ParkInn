6af04a44577a9960c90bf470f694343f4d2ec53a
NaPopravkuYurii <megafonka@yandex.ru>
2015-01-20 15:19:19 +0400

    .

---
+++ logicasoft.reviews/install/version.php
@@ -0,0 +1,6 @@
+<?
+$arModuleVersion = array(
+	"VERSION" => "1.0.0",
+	"VERSION_DATE" => "2015-01-15 19:58:11"
+);
+?>
---
+++ logicasoft.reviews/lang/ru/install/index.php
@@ -0,0 +1,6 @@
+<?
+$MESS["logicasoft.reviews_MODULE_NAME"] = "Выгрузка отзывов";
+$MESS["logicasoft.reviews_MODULE_DESC"] = "Выгрузка отзывов";
+$MESS["logicasoft.reviews_PARTNER_NAME"] = "LogicaSoft";
+$MESS["logicasoft.reviews_PARTNER_URI"] = "http://www.logicasoft.pro";
+?>
